2e80e6f2ddaf8bacc1fd729c0b4cdba1be0a9510
Sichkar Yurii <si4kar2017@gmail.com>
2022-05-16 20:58:06 +0300

    Lesson_15

--- script/scripts.js
+++ script/scripts.js
@@ -1,125 +1,45 @@
-const DELETE_BTN_CLASS = 'delete-btn';
-const INPUT_STRING = 'list_elements';
-const STORAGE_KEY = 'contactList';
+const STORAGE_KEY = 'usersList';
 
 const inputForm = document.querySelector('.input_block');
-const contactListEl = document.querySelector('.input_blocks');
+const usersListEl = document.querySelector('.input_blocks');
 const listTemplate = document.querySelector('.list_template').innerHTML;
-const inputContactsEl = document.querySelectorAll('.input_elements');
-const errorText = document.querySelector('.error');
-const addBtn = document.querySelector('.input_btn');
-const resultEl = document.querySelector('.input_blocks');
 
-inputForm.addEventListener('submit', onContactFormSubmit);
-contactListEl.addEventListener('click', onContactsListClick);
+inputForm.addEventListener('submit', onUserFormSubmit);
 
-let contactList = [];
+let usersList = {};
 
 init();
 
-function onContactFormSubmit(e) {
-    e.preventDefault();
-
-    const newContact = getContact();
-
-    if (isContactElValid(newContact)) {
-        addContact(newContact);
-        resetForm();
-        removeError();
-    } else {
-        showError();
-    }
-}
-
-function getContact() {
-    const contact = {};
-
-    inputContactsEl.forEach((inp) => {
-        contact[inp.name] = inp.value;
-    });
-
-    return contact;
-}
-
-function generateContactHtml(contact) {
-    return listTemplate.replace('{{id}}', contact.id)
-                        .replace('{{Name}}', contact.name)
-                        .replace('{{Surname}}', contact.email)
-                        .replace('{{Phone_number}}', contact.phone)
-                        .replace('{{Address}}', contact.address.city + ', ' + contact.address.street);
-}
-
-function isContactElValid(contact) {
-    return validateInput(contact.name)
-        && validateInput(contact.email)
-        && validateInput(contact.phone);
-}
-
-function validateInput(value) {
-    return value !== '';
-}
-
-function addContact(contact) {
-    const newContactHtml = generateContactHtml(contact);
-    contactListEl.insertAdjacentHTML('beforeend', newContactHtml);
-
-    contactList.push(contact);
-    contact.id = Date.now();
-
-    saveData();
-    renderList();
-}
-
 function init() {
-    // contactList = restoreData();
     fetch('https://jsonplaceholder.typicode.com/users')
         .then((res) => res.json())
         .then((data) => {
-            contactList = data;
+            usersList = data;
             renderList();        
     })
 }
 
-function getContactID(el) {
-    return +el.closest('.' + INPUT_STRING).dataset.id;
+function onUserFormSubmit() {
+    let newUser;
+    addUser(newUser);
 }
 
-function onContactsListClick(e) {
-    if (e.target.classList.contains(DELETE_BTN_CLASS)) {
-        contactID = getContactID(e.target);
-        deleteСontact(contactID);
-    }   
+function generateUserHtml(user) {
+    return listTemplate.replace('{{Name}}', user.name)
+                        .replace('{{Surname}}', user.email)
+                        .replace('{{Phone_number}}', user.phone)
+                        .replace('{{Address}}', user.address.city + ', ' + user.address.street);
 }
 
-function renderList() {
-    contactListEl.innerHTML = contactList.map(generateContactHtml).join('\n');
-}
+function addUser(user) {
+    const newUserHtml = generateUserHtml(user);
+    usersListEl.insertAdjacentHTML('beforeend', newUserHtml);
 
-function resetForm() {
-    inputForm.reset();
-}
+    usersList.push(user);
 
-function showError() {
-    errorText.classList.add('show');
-}
-
-function removeError() {
-    errorText.classList.remove('show');
-}
-
-function deleteСontact(id) {
-    contactList = contactList.filter((contact) => contact.id !== id);
-
-    saveData();
     renderList();
 }
 
-function saveData() {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(contactList));
+function renderList() {
+    usersListEl.innerHTML = usersList.map(generateUserHtml).join('\n');
 }
-
-// function restoreData() {
-//     const data = localStorage.getItem(STORAGE_KEY);
-
-//     return data ? JSON.parse(data) : [];
-// }
